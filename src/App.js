7497bca5ae4d91131ec8395eb2dc6d066f7b9599
akshaychittoor <akshaychittor9048@gmail.com>
2023-11-24 10:20:33 +0530

    validation implemented

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ function App() {
   const[Interest,setInterest]=useState(0);
   const[Rate,setRate]=useState(0);
   const[Year,setYear]=useState(0);
+  const[isPrinciple,setIsPrinciple]=useState(true)
+  const[isRate,setIsRate]=useState(true)
+  const[isYear,setIsYear]=useState(true)
 
   const handleSubmit=(e)=>{
     e.preventDefault()
@@ -41,7 +44,45 @@ function App() {
      setRate(0);
      setYear(0);
   }
-  return (
+  const validate=(e)=>{
+    const{value,name}=e.target;
+    console.log(value);
+    // regualer expression: to check whether a given string  has particular pattern 
+    // should have forward salsh at the beginning and end 
+    // start of the expression is indicated by ^ (raised) 
+    // ending of the expression is indicatedby $
+    // if it match, we get array as return else null
+    // !! is used to convert result of regulart expression to boolean value
+    if(!!value.match(/^[0-9]+$/)){
+      if (name === 'principle') {
+       setPrinciple(value);
+       setIsPrinciple(true);
+    }
+    else if (name === 'rate') {
+      setRate(value);
+      setIsRate(true);
+    }
+    else{
+      setYear(value);
+      setIsYear(true);
+    }
+  }
+    else{
+      if (name === 'principle') {
+      setPrinciple(value);
+      setIsPrinciple(false)
+    }
+    else if (name === 'rate') {
+      setRate(value);
+      setIsRate(false);
+    }
+    else{
+      setYear(value);
+      setIsYear(false);
+    }
+  }
+  }
+ return (
     <div className='d-flex justify-content-center align-items-center w-100 mt-5' style={{ height:"85vh" }}>
       <div style={{ width: "500px" }} className='bg-light p-5 rounded'>
         <h3>Simple Interest</h3>
@@ -53,20 +94,35 @@ function App() {
         <form action="" className='mt-5' onSubmit={(e)=>handleSubmit(e)}>
 
           <div className='mb-3'>
-            <TextField id="outlined-basic" label="Principle Amount" variant="outlined" className='w-100' value={Principle} onChange={(e)=>setPrinciple(e.target.value)} />
+            <TextField name='principle' id="outlined-basic" label="Principle Amount" variant="outlined" className='w-100' value={Principle} onChange={(e)=>validate(e)} />
           </div>
+          {! isPrinciple &&
+          <div>
+             <p className='text-danger'>Invalid Input</p>
+          </div>
+          }
 
           <div className=''>
-            <TextField id="outlined-basic" label="Rate of Interest (pa)%" variant="outlined" className='w-100' value={Rate} onChange={(e)=>setRate(e.target.value)} />
+            <TextField name='rate' id="outlined-basic" label="Rate of Interest (pa)%" variant="outlined" className='w-100' value={Rate} onChange={(e)=>validate(e)} />
           </div>
+          {! isRate &&
+          <div>
+             <p className='text-danger'>Invalid Input</p>
+          </div>
+          }
 
           <div className='mt-3'>
-            <TextField id="outlined-basic" label="Year(Yr)" variant="outlined" className='w-100' value={Year} onChange={(e)=>setYear(e.target.value)} />
+            <TextField  id="outlined-basic" label="Year(Yr)" variant="outlined" className='w-100' value={Year} onChange={(e)=>validate(e)} />
           </div>
+          {! isYear &&
+          <div>
+             <p className='text-danger'>Invalid Input</p>
+          </div>
+          }
 
           <div className='mt-5'>
             <Stack direction="row" spacing={2}>
-            <Button style={{height:"50px", width:"200px"}} variant="contained" className='bg-success border' type='submit'>CALCULATE</Button>
+            <Button disabled={!isPrinciple || !isRate || !isYear} style={{height:"50px", width:"200px"}} variant="contained" className='bg-success border' type='submit'>CALCULATE</Button>
             <Button style={{height:"50px", width:"200px"}} variant="contained" onClick={clearValues} >RESET</Button>
             </Stack>
           </div>
